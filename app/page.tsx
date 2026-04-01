18cf3d8fd960ec2b34d586becdcc129d184d5dd2
sonofwo1f <c3l0717@gmail.com>
2026-04-01 10:22:51 -0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getDashboardData } from "@/lib/data";
-import { TrackerApp } from "@/components/TrackerApp";
-
-export const dynamic = "force-dynamic";
-
-export default async function HomePage() {
-  const data = await getDashboardData();
-  return <TrackerApp initialData={data} />;
-}
